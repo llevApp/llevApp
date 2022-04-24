7bf2121b7cdbf19259bc029807b9e4aba2b82d22
ssp013 <ssp013sanchez@gmail.com>
2022-04-24 12:42:28 -0400

    Re init main

--- App.js
+++ App.js
@@ -1,12 +1,20 @@
+import * as React from 'react';
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
-
+import { NavigationContainer } from '@react-navigation/native';
 export default function App() {
   return (
+    <NavigationContainer>
     <View style={styles.container}>
-      <Text>Hola mundo!</Text>
+      <Text>Hola mundsdasdaso!</Text>
+      <Text>Hola mundsdasdaso!</Text>
+      <Text>Hola mundsdasdaso!</Text>
+      <Text>Hola mundsdasdaso!</Text>
+      <Text>Hola mundsdasdaso!</Text>
+      <Text>Hola mundsdasdaso!</Text>
       <StatusBar style="auto" />
     </View>
+    </NavigationContainer>
   );
 }
 
