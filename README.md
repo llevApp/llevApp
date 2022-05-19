0ac3a27bb7e66b7e2eef51eed2738a5bd3f43d81
ssp013 <ssp013sanchez@gmail.com>
2022-05-18 22:33:39 -0400

    fix

--- README.md
+++
@@ -1,2 +0,0 @@
-# llevApp
-Code of the frontend development of the llevApp application in react native
--- src/pages/Modules/Driver/Driver.js
+++
@@ -1,9 +0,0 @@
-import FooterTab from '../../../ui/FooterTab/FooterTab'
-import HomeDriver from '../../../pages/Modules/Driver/HomeDriver/HomeDriver'
-import MainContainer from './MainContainer'
-import { Text } from 'native-base'
-export default function Driver() {
-    return <>
-    <MainContainer></MainContainer>
-    </>
-}
