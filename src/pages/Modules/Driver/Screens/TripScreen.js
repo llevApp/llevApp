6fabc882aeedf0efa164bac89fb9cf2fb96a96f4
Nicolás García Santelices <nicolas.garcia.s@etrans.cl>
2022-07-04 23:12:30 -0400

    update historial de viajes

--- src/pages/Modules/Driver/Screens/TripScreen.js
+++ src/pages/Modules/Driver/Screens/TripScreen.js
@@ -4,12 +4,68 @@ import { useUserStore } from './../../../Home/Store/StoreHome';
 import { useTripsStore } from './StoreTrip/StoreTrips';
 import moment from 'moment';
 import { MaterialCommunityIcons } from '@expo/vector-icons'; 
+import { FontAwesome } from '@expo/vector-icons';
 import {URL_API,TRIPS_DRIVER} from '@env';
 export default function TripScreen({ navigation }) {
 
     const {tripsArray} = useTripsStore()
     const { idUser, name } = useUserStore();
   
+
+
+    const passengerNumberWidget =(item)=>{
+        if (item.passengerNumber == 0){
+            return(
+                <HStack marginTop={8} marginRight={4}>
+                    <FontAwesome name="user-o" size={24} color="black" />
+                    <FontAwesome name="user-o" size={24} color="black" />
+                    <FontAwesome name="user-o" size={24} color="black" />
+                    <FontAwesome name="user-o" size={24} color="black" />
+                </HStack>
+            )
+        }
+        if (item.passengerNumber == 1){
+            return(
+                <HStack marginTop={8} marginRight={4}>
+                    <FontAwesome name="user" size={26} color="black" />
+                    <FontAwesome name="user-o" size={24} color="black" />
+                    <FontAwesome name="user-o" size={24} color="black" />
+                    <FontAwesome name="user-o" size={24} color="black" />
+                </HStack>
+            )
+        }
+        if (item.passengerNumber == 2){
+            return(
+                <HStack marginTop={8} marginRight={4}>
+                    <FontAwesome name="user" size={26} color="black" />
+                    <FontAwesome name="user" size={26} color="black" />
+                    <FontAwesome name="user-o" size={24} color="black" />
+                    <FontAwesome name="user-o" size={24} color="black" />
+                </HStack>
+            )
+        }
+        if (item.passengerNumber == 3){
+            return(
+                <HStack marginTop={8} marginRight={4}>
+                    <FontAwesome name="user" size={26} color="black" />
+                    <FontAwesome name="user" size={26} color="black" />
+                    <FontAwesome name="user" size={26} color="black" />
+                    <FontAwesome name="user-o" size={24} color="black" />
+                </HStack>
+            )
+        }
+        if (item.passengerNumber == 4){
+            return(
+                <HStack marginTop={8} marginRight={4}>
+                    <FontAwesome name="user" size={26} color="black" />
+                    <FontAwesome name="user" size={26} color="black" />
+                    <FontAwesome name="user" size={26} color="black" />
+                    <FontAwesome name="user" size={26} color="black" />
+                </HStack>
+            )
+        }
+    }
+
     useEffect(()=>{useTripsStore.getState().setTripsPassenger(idUser)},[idUser]);
     useEffect(()=>{
         useTripsStore.getState().setTripsPassenger(idUser)
@@ -48,32 +104,37 @@ export default function TripScreen({ navigation }) {
                         <FlatList data={tripsArray} 
                         renderItem={({item}) => 
                             <Box borderWidth="0.5" borderColor="gray.200" _dark={{borderColor: "gray.600"} } background="white" borderRadius="20" w={"100%"} h={120}>
-                                <HStack space={3} justifyContent="flex-end" margin={5} marginTop={2} marginBottom={2}>
+                                <HStack space={4} justifyContent="flex-end" marginRight={5} marginLeft={10} marginTop={2}>
                                     <VStack marginLeft ="2" >
-                                        <HStack space={2} >
-                                            <Text fontSize="sm"  _dark={{color: "warmGray.50"}} color="#159A9C" maxWidth={220} minWidth={220} >
+                                        <HStack  >
+                                            <Text fontSize="sm"  _dark={{color: "warmGray.50"}} color="#159A9C" maxWidth={120} minWidth={120} >
                                                 {item.address?.toUpperCase()}
                                             </Text>
                                         </HStack>
-                                        <HStack space={3} marginTop={2} marginBottom={2}>
-                                            <Text  fontSize="5xl" color="#6AC18A" _dark={{color: "warmGray.200"}}>
+                                        <HStack space={3} maxH={10} minH={10}>
+                                            <Text  fontSize="4xl" color="#6AC18A" _dark={{color: "warmGray.200"}}>
                                                 $
                                             </Text>
-                                            <Text  fontSize="5xl" color="#6AC18A" _dark={{color: "warmGray.200"}} >
+                                            <Text  fontSize="4xl" color="#6AC18A" _dark={{color: "warmGray.200"}} >
                                                 {item.recentText}
                                             </Text>
-                                            <Text  fontSize="xs" color="coolGray.600" _dark={{color: "warmGray.200"}} marginTop="6" >
+                                            <Text  fontSize="2xs" color="coolGray.600" _dark={{color: "warmGray.200"}} marginTop="6" >
                                                 USD
                                             </Text>
                                         </HStack>
+    
                                     </VStack>
                                     <Spacer />
-                                    <HStack space={1}  >
+                                    <VStack marginRight={10}>
+                                    <HStack space={1}>
                                         <Text fontSize="2xs" _dark={{color: "warmGray.50"}} color="coolGray.800" >
                                             {item.timeStamp}
                                         </Text>
                                     </HStack>
+                                        {passengerNumberWidget(item)}
+                                    </VStack>
                                 </HStack>
+                                
                             </Box>} keyExtractor={item => item.id}
                         />
                     </>) : 
